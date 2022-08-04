2c9525cdc0f1de078fb63111831fc30c2dc5dab3
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-08-04 22:14:32 +0000

    Test for key wrapping back to start of alphabet.

--- test_caesar.py
+++ test_caesar.py
@@ -4,3 +4,7 @@ import caesar
 class TestCipherMethods(unittest.TestCase):
 	def test_encrypt_happypath(self):
 		self.assertEqual(caesar.encrypt("caesar"), "fdhvdu")
+
+	def test_encrypt_shift_wraps(self):
+		self.assertEqual(caesar.encrypt("zoo"), "crr")
+
