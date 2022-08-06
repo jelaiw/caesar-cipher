51d3895dece09a7fa17ab30d197df4a7458ecdbb
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-08-06 22:34:41 +0000

    Figure out session state.

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
 import streamlit as st
 import caesar
 
+def encrypt_handler(plaintext):
+	ciphertext = caesar.encrypt(plaintext)
+	st.session_state.plaintext = plaintext
+	st.session_state.ciphertext = ciphertext
+
+def decrypt_handler(ciphertext):
+	plaintext = caesar.decrypt(ciphertext)
+	st.session_state.plaintext = plaintext
+	st.session_state.ciphertext = ciphertext
+
 st.title("The Caesar Cipher")
 
-plaintext = st.text_area("Plaintext")
+st.header("Try it!")
 
-if st.button("Encrypt"):
-	ciphertext = caesar.encrypt(plaintext)
-	st.text_area("Ciphertext", ciphertext)
+col1, col2 = st.columns(2)
+
+with col1:
+	previous_plaintext = ""
+	if 'plaintext' in st.session_state:
+		previous_plaintext = st.session_state.plaintext
+	plaintext = st.text_area("Plaintext", value=previous_plaintext)
+	st.button("Encrypt", on_click=encrypt_handler, args=(plaintext, ))
+
+with col2:
+	previous_ciphertext = ""
+	if 'ciphertext' in st.session_state:
+		previous_ciphertext = st.session_state.ciphertext
+	ciphertext = st.text_area("Ciphertext", value=previous_ciphertext)
+	st.button("Decrypt", on_click=decrypt_handler, args=(ciphertext, ))
