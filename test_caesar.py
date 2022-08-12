51f5932a3e10fc1261fc847b091dadf891079c06
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-08-12 16:43:27 +0000

    Group related unit tests together.

--- test_caesar.py
+++ test_caesar.py
@@ -5,20 +5,20 @@ class TestCipherMethods(unittest.TestCase):
 	def test_encrypt_happypath(self):
 		self.assertEqual(caesar.encrypt("caesar"), "fdhvdu")
 
-	def test_encrypt_shift_wraps(self):
-		self.assertEqual(caesar.encrypt("zoo"), "crr")
-
-	def test_encrypt_exact_shift_boundary(self):
-		self.assertEqual(caesar.encrypt("xray"), "audb")
-
 	def test_decrypt_happypath(self):
 		self.assertEqual(caesar.decrypt("fdhvdu"), "caesar")
+
+	def test_encrypt_shift_wraps(self):
+		self.assertEqual(caesar.encrypt("zoo"), "crr")
 
 	def test_decrypt_shift_wraps(self):
 		self.assertEqual(caesar.decrypt("crr"), "zoo")
 
 	def test_decrypt_shift_wraps_2(self):
 		self.assertEqual(caesar.decrypt("bar"), "yxo")
+
+	def test_encrypt_exact_shift_boundary(self):
+		self.assertEqual(caesar.encrypt("xray"), "audb")
 
 	def test_decrypt_exact_shift_boundary(self):
 		self.assertEqual(caesar.decrypt("audb"), "xray")
