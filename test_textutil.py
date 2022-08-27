5d9cc3bb7dd8b4d56950f3ebda38a40a2cd1be07
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-08-27 18:31:08 +0000

    Add disguise_text() for fun.

--- test_textutil.py
+++ test_textutil.py
@@ -1,6 +1,7 @@
 import unittest
 from textutil import remove_whitespace
 from textutil import remove_punctuation
+from textutil import disguise_text
 
 class TestTextUtilMethods(unittest.TestCase):
 	def test_remove_whitespace_happypath(self):
@@ -8,3 +9,7 @@ class TestTextUtilMethods(unittest.TestCase):
 
 	def test_remove_punctuation_happypath(self):
 		self.assertEqual(remove_punctuation("we.the people!"), "wethe people")
+
+	def test_disguise_text_happypath(self):
+		# Borrow example from https://en.wikipedia.org/wiki/Substitution_cipher.
+		self.assertEqual(disguise_text("SIAA ZQ LKBA. VA ZOA RFPBLUAOAR!"), "SIAAZ QLKBA VAZOA RFPBL UAOAR")
--- textutil.py
+++ textutil.py
@@ -1,4 +1,14 @@
 import string
+import re
+
+# Disguise text by omitting punctuation and spaces and re-writing in blocks of fixed length (five character groups).
+# See https://en.wikipedia.org/wiki/Substitution_cipher for further context.
+def disguise_text(text):
+	text = remove_whitespace(text)
+	text = remove_punctuation(text)
+
+	BLOCKS = re.compile(".....?").findall
+	return " ".join(BLOCKS(text))
 
 def remove_whitespace(text):
 	return "".join(text.split())
