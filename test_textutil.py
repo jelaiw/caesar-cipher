183e984fc61dbb9eabb4886739034aaf00476a9a
Jelai Wang <14236583+jelaiw@users.noreply.github.com>
2022-08-27 18:22:59 +0000

    Add remove_punctuation() to textutil module.

--- test_textutil.py
+++ test_textutil.py
@@ -1,6 +1,10 @@
 import unittest
 from textutil import remove_whitespace
+from textutil import remove_punctuation
 
 class TestTextUtilMethods(unittest.TestCase):
 	def test_remove_whitespace_happypath(self):
 		self.assertEqual(remove_whitespace("we the people"), "wethepeople")
+
+	def test_remove_punctuation_happypath(self):
+		self.assertEqual(remove_punctuation("we.the people!"), "wethe people")
--- textutil.py
+++ textutil.py
@@ -1,2 +1,9 @@
+import string
+
 def remove_whitespace(text):
 	return "".join(text.split())
+
+# See https://docs.python.org/3/library/string.html#string.punctuation.
+# See https://stackoverflow.com/a/60725620.
+def remove_punctuation(text):
+	return text.translate(str.maketrans("", "", string.punctuation))
