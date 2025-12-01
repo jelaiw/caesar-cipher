343ddf9938b9b394290765e0e045294b88ceb4a5
jelaiw <14236583+jelaiw@users.noreply.github.com>
2025-12-01 23:22:19 +0000

    Move fig_1-2.png to assets/ for consistency.

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ tab1, tab2, tab3 = st.tabs(['Caesar cipher', 'ROT13', 'The Basics'])
 with tab1:
     st.subheader("Caesar cipher")
     st.markdown("\"The Caesar cipher ... encrypts a message by shifting each of the letters down three positions in the alphabet, wrapping back around to A if the shift reaches Z\" (Aumasson 2018).")
-    st.image("fig_1-2.png", width=384, caption="Figure 1: Caesar cipher example")
+    st.image("assets/fig_1-2.png", width=384, caption="Figure 1: Caesar cipher example")
 
 with tab2:
     st.subheader("ROT13")
